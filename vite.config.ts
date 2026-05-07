95a5309a19f4d2b1563a2d28806a3a5b0551799e
Maximilian PINDER-WHITE <mpinderwhite@proton.me>
2026-05-07 09:43:32 +0200

    feat: add gh deployment ci

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,19 @@ import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
+  base: '/ynov-cicd/',
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [
+    devtools(),
+    tailwindcss(),
+    tanstackStart({
+      prerender: {
+        enabled: true,
+        autoSubfolderIndex: true,
+      },
+    }),
+    viteReact(),
+  ],
 })
 
 export default config
